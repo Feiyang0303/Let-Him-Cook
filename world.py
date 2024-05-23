c50361b13917c947b47519f2c1d4903ebb4e61cf
Kevin Jin <86444429+wabubby@users.noreply.github.com>
2024-05-23 13:30:21 -0400

    Structure

---
+++ world.py
@@ -0,0 +1,18 @@
+
+class World:
+    pass
+
+class Tile:
+    pass
+
+class Building:
+    pass
+
+class Shop(Building):
+    pass
+
+class Storage(Building):
+    pass
+
+class Processor(Building):
+    pass
