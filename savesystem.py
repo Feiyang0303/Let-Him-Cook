ba1e010b37d1b17cdfc33df69b332f3f2e5d61a7
blizzardlizzer <michaelchxu@gmail.com>
2024-05-24 14:07:53 -0400

    Update savesystem.py

--- savesystem.py
+++ savesystem.py
@@ -1,1 +1,18 @@
 import json
+
+def save_game_state():
+    game_state = {
+        
+    }
+
+    with open('gamesave.json','w') as f:
+     json.dump(game_state,f)
+
+def load_game_state():
+    try:
+      with open('gamesave.json','r') as f:
+         game_state = json.load(f)
+         return game_state
+    except FileNotFoundError:
+      print("Save File not Found")
+      return None
