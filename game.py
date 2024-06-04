0bfadca2e9b249cb718ed6746e8f9b0a4fc1963a
Kevin Jin <86444429+wabubby@users.noreply.github.com>
2024-06-04 11:23:19 -0400

    Deleted "Game" Interaction
    deleted interact interaction in the game class

--- game.py
+++ game.py
@@ -70,11 +70,6 @@ class Game:
                     self.isPaused = not self.isPaused
                 elif event.key == pg.K_i:
                     self.state = INVENTORY_STATE if self.state == PLAY_STATE else PLAY_STATE
-                elif event.key == pg.K_e:
-                    player_pos = self.player.get_pos()
-                    tile = self.world.get(player_pos.x, player_pos.y)
-                    if isinstance(tile, Fridge):
-                        tile.interact()
             # elif event.type == pg.VIDEOEXPOSE:
             #     print("MOVIGN WINDOW BEEP BEEP")
             #     self.lagCompensation = False
