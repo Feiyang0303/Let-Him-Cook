00d266951548d44bd708145256110404781c6722
Feiyang <128330965+Feiyang0303@users.noreply.github.com>
2024-06-06 13:30:35 -0400

    Update game.py

--- game.py
+++ game.py
@@ -12,13 +12,14 @@ from items import *
 from worldrenderer import *
 from preferencescreen import *
 
+
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
         self.FPS = 60
-        self.DT = 1/self.FPS
+        self.DT = 1 / self.FPS
         self.clock = pg.time.Clock()
         self.storage = Storage()
 
@@ -40,25 +41,29 @@ class Game:
         self.tile_library = self.world.tile_library
 
         self.pause_screen = PreferenceScreen(self)
+        self.debug_button = BuyStructureButton(self, pg.Rect(SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2, TILE_WIDTH * 2,
+                                                            TILE_HEIGHT * 2), "counter")
 
-        # UI
-        self.buyMenu = BuyMenu(self, 200, 200)
-        self.debug_button = BuyStructureButton(self, pg.Rect(SCREEN_HEIGHT/2, SCREEN_WIDTH/2, TILE_WIDTH*2, TILE_HEIGHT*2), "counter")
+        self.scoreText = Text(self, "fonts/pixel-bit-advanced.ttf", 24, (255, 255, 255), pg.Vector2(MARGIN, MARGIN),
+                              text=f"${AERSOL}")
 
-        self.scoreText = Text(self, "fonts/pixel-bit-advanced.ttf", 24, (255, 255, 255), pg.Vector2(MARGIN, MARGIN), text=f"${AERSOL}")
+        self.timerText = Text(self, "fonts/pixel-bit-advanced.ttf", 24, (255, 255, 255),
+                              pg.Vector2(SCREEN_WIDTH - MARGIN, MARGIN), justification=JUSTIFY_RIGHT, text="1:00")
 
-        self.timerText = Text(self, "fonts/pixel-bit-advanced.ttf", 24, (255, 255, 255), pg.Vector2(SCREEN_WIDTH - MARGIN, MARGIN), justification=JUSTIFY_RIGHT, text="1:00")
 
     def load_game(self):
-        self.new_game()
-        load_game_state(self)
+            self.new_game()
+            load_game_state(self)
+
 
     def set_game_state(self, state):
         self.state = state
         self.isFreezed = self.state != PLAY_STATE
-    
+
+
     def check_freeze(self):
         self.isFreezed = self.isPaused or (self.state != PLAY_STATE)
+
 
     def events(self):
         self.lagCompensation = True
@@ -72,31 +77,24 @@ class Game:
                 if event.key == pg.K_ESCAPE:
                     self.isPaused = not self.isPaused
 
-            # elif event.type == pg.VIDEOEXPOSE:
-            #     print("MOVIGN WINDOW BEEP BEEP")
-            #     self.lagCompensation = False
-            #     self.DT = 1/self.FPS
-                elif event.key == pg.K_i:
-                    self.state = INVENTORY_STATE if self.state == PLAY_STATE else PLAY_STATE
-
-                elif event.key == pg.K_i:
-                    self.state = INVENTORY_STATE if self.state == PLAY_STATE else PLAY_STATE
-
             for eventee in self.eventees:
                 eventee.callEvent(event)
+
 
     def update(self):
         self.world.update()
         self.player.update()
-        self.buyMenu.update()
+        self.debug_button.update()
         self.world_editor.update()
         self.scoreText.set_text(f"${self.money}")
-    
+
+
     def immuneUpdate(self):
         self.world.immuneUpdate()
         self.player.immuneUpdate()
-        self.buyMenu.immuneUpdate()
+        self.debug_button.immuneUpdate()
         self.world_editor.immuneUpdate()
+
 
     def draw(self):
         self.screen.fill(BACKGROUND_COLOUR)
@@ -104,7 +102,6 @@ class Game:
         self.player.draw()
         self.world_renderer.draw()
         self.world_editor.draw()
-
 
         self.scoreText.draw()
         self.timerText.draw()
@@ -115,11 +112,12 @@ class Game:
 
         pg.display.update()
 
+
     def run(self):
         while True:
             self.events()
-            self.DT = min(self.clock.tick(self.FPS) / 1000, 1/12)
-            
+            self.DT = min(self.clock.tick(self.FPS) / 1000, 1 / 12)
+
             self.check_freeze()
             if not self.isFreezed:
                 self.update()
